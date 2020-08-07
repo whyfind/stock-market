570d1f88a26f55dc39e4002ed3f0792a0e47bb41
wangfei <wangfei@eshippinggateway.com>
2020-08-07 18:03:24 +0800

    //

--- index.js
+++ index.js
@@ -3,32 +3,57 @@ console.log('你好啊！my name is dorsey');
 const https = require('https');
 const cheerio = require('cheerio');
 
-let url = 'https://sns.sseinfo.com/ajax/feeds.do?page=1&type=10&pageSize=10&lastid=-1&show=1&_=1596535665526';
+let url = 'https://sns.sseinfo.com/ajax/feeds.do?type=11&pageSize=10&lastid=-1&show=1&page=1&_=' + new Date().getTime();
 
-var htmlTest = 'ceshi'
-https.get(url, function (res) {
-    let chunks = [],
-        size = 0;
-    res.on('data', function (chunk) {
-        chunks.push(chunk);
-        size += chunk.length;
+var  script = "<script>Notification.requestPermission(res =>{ console.log(res); });function createNotify(title,options) {\n" +
+    "\n" +
+    "    var PERMISSON_GRANTED = 'granted';\n" +
+    "    var PERMISSON_DENIED = 'denied';\n" +
+    "    var PERMISSON_DEFAULT = 'default';\n" +
+    "\n" +
+    "    if (Notification.permission === PERMISSON_GRANTED) {\n" +
+    "        notify(title,options);\n" +
+    "    } else {\n" +
+    "        Notification.requestPermission(function (res) {\n" +
+    "            if (res === PERMISSON_GRANTED) {\n" +
+    "                notify(title,options);\n" +
+    "            }\n" +
+    "        });\n" +
+    "    }\n" +
+    "\n" +
+    "    function notify($title,$options) {\n" +
+    "        var notification = new Notification($title, $options);\n" +
+    "    }\n" +
+    "};setTimeout(function(){createNotify('测试通知',{body:'啊啊啊啊啊啊啊啊啊啊啊啊'})}, 10000)</script>"
+
+
+var htmlTest = ''
+var getHtml = function (callback) {
+    https.get(url, function (res) {
+        let chunks = [],
+            size = 0;
+        res.on('data', function (chunk) {
+            chunks.push(chunk);
+            size += chunk.length;
+        });
+
+        res.on('end', function () {
+            console.log('数据包传输完毕');
+            let data = Buffer.concat(chunks, size);
+            let html = data.toString();
+
+            let $ = cheerio.load(html);
+            var arr = []
+            $('.m_feed_item').each(function (index,v) {
+                arr.push('<p>' + '<h1>' +$(v).find('.m_qa_detail .m_feed_txt a').html()+ '</h1>' + $(v).find('.m_qa .m_feed_txt').html()+'</p>')
+            })
+            let result = [];
+            htmlTest = arr.join('') + script
+
+            callback()
+        });
     });
-
-    res.on('end', function () {
-        console.log('数据包传输完毕');
-        let data = Buffer.concat(chunks, size);
-        let html = data.toString();
-
-        let $ = cheerio.load(html);
-        var arr = []
-        $('#feedall').find('.m_feed_item').each(function (index,v) {
-            arr.push('<p>' + $(v).find('.m_feed_txt').html()+'</p>')
-        })
-        let result = [];
-        console.log(arr.join(''))
-        htmlTest = 'ces'
-    });
-});
+}
 
 
 var express = require('express');
@@ -38,7 +63,11 @@ app.use('/public', express.static('public'));
 //  主页输出 "Hello World"
 app.get('/', function (req, res) {
     console.log("主页 GET 请求");
-    res.send(htmlTest);
+    getHtml(
+        function () {
+            res.send(htmlTest);
+        }
+    )
 })
 
 
