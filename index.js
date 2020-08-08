067c50702d9b81352eb155f8f126a9eefe0de787
whyfind <927230462@qq.com>
2020-08-08 16:41:02 +0800

    /

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ console.log('你好啊！my name is dorsey');
 
 const https = require('https');
 const cheerio = require('cheerio');
+const request = require('request');
+var querystring = require('querystring');
+var fs = require("fs")
 
-let url = 'https://sns.sseinfo.com/ajax/feeds.do?type=11&pageSize=10&lastid=-1&show=1&page=1&_=' + new Date().getTime();
 
-var  script = "<script>Notification.requestPermission(res =>{ console.log(res); });function createNotify(title,options) {\n" +
+
+var createNotify = "<script>setTimeout(function(){window.location.reload()},60*1000);" +
+"Notification.requestPermission(res =>{ console.log(res); });"+
+"function createNotify(title,options) {\n" +
     "\n" +
     "    var PERMISSON_GRANTED = 'granted';\n" +
     "    var PERMISSON_DENIED = 'denied';\n" +
@@ -24,11 +29,84 @@ var  script = "<script>Notification.requestPermission(res =>{ console.log(res); 
     "    function notify($title,$options) {\n" +
     "        var notification = new Notification($title, $options);\n" +
     "    }\n" +
-    "};setTimeout(function(){createNotify('测试通知',{body:'啊啊啊啊啊啊啊啊啊啊啊啊'})}, 10000)</script>"
+    "};</script>"
 
 
 var htmlTest = ''
-var getHtml = function (callback) {
+
+var getFilter = function(){
+    var data = ''
+    try{
+        data = fs.readFileSync('filter.txt');
+        data = data.toString()
+    }catch{
+        data = ''
+    }
+    return data
+}
+
+var getHistoryId = function(){
+    var data = {}
+    try{
+        data = fs.readFileSync('historyId.json');
+        data = data.toString()
+        data = JSON.parse(data)
+    }catch{
+        data = {}
+    }
+
+    return data
+}
+
+var getFilterArray = function(){
+    var str = getFilter()
+    str = str.split(',')
+    return str
+}
+
+var renderListFilter = function(list){
+    var arr = []
+    var notification = "<script>"
+    var historyId = getHistoryId()
+    list.forEach(function (v, index) {
+        var renderPush = function(v){
+            var code = v.code ? '('+v.code +')' : ''
+            arr.push('<p>' + '<h1>' + v.title + code +'</h1>' + v.text +'</p>')
+        }
+        var notify = function(v){
+            historyId[v.type] = historyId[v.type] || []
+            historyId[v.type].push(v.id)
+            notification += "setTimeout(function(){createNotify('"+ v.title +"',{body:'有需要您关注的信息'})}, 0);"
+        }
+        if(!getFilter()){
+            renderPush(v)
+        }else{
+           var str = getFilterArray()
+           str.forEach(function(item){
+                if(v.text.indexOf(item) >= 0){
+                    renderPush(v)
+                    //没有历史记录 或者 没有历史通知
+                    if(!historyId[v.type] || historyId[v.type].indexOf(v.id) < 0){
+                        notify(v)
+                    }
+                }
+           })
+        }
+    })
+    notification += "</script>"
+    fs.writeFile('historyId.json', JSON.stringify(historyId),  function(err) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log("数据写入成功！");
+        console.log("--------我是分割线-------------")
+        console.log("读取写入的数据！");
+    });
+    htmlTest = arr.length ? (arr.join('') + createNotify + notification) : '<h1 style="text-align: center;">暂无关键字匹配的数据。</h1>';
+}
+
+var getShangHaiHtml = function (callback) {
+    let url = 'https://sns.sseinfo.com/ajax/feeds.do?type=11&pageSize=10&lastid=-1&show=1&page=1&_=' + new Date().getTime();
     https.get(url, function (res) {
         let chunks = [],
             size = 0;
@@ -41,18 +119,56 @@ var getHtml = function (callback) {
             console.log('数据包传输完毕');
             let data = Buffer.concat(chunks, size);
             let html = data.toString();
-
-            let $ = cheerio.load(html);
-            var arr = []
+            let $ = cheerio.load(html, { decodeEntities: false });
+            var list = []
             $('.m_feed_item').each(function (index,v) {
-                arr.push('<p>' + '<h1>' +$(v).find('.m_qa_detail .m_feed_txt a').html()+ '</h1>' + $(v).find('.m_qa .m_feed_txt').html()+'</p>')
+                list.push({
+                    title: $(v).find('.m_qa_detail .m_feed_txt a').html(),
+                    text: $(v).find('.m_qa .m_feed_txt').html(),
+                    id: $(v).find('.m_qa .m_feed_txt').attr('id').split('-')[1],
+                    type: 'ShangHai'
+                })
             })
-            let result = [];
-            htmlTest = arr.join('') + script
-
+            renderListFilter(list)
             callback()
         });
     });
+}
+
+var getShenZhenHtml = function (callback) {
+    var data = {
+        pageNo: 1,
+        pageSize: 10,
+        searchTypes: '11,11,',
+        market: '',
+        industry: '',
+        stockCode: ''
+    }
+    request({
+        url: 'http://irm.cninfo.com.cn/ircs/index/search',//请求路径
+        method: "POST",//请求方式，默认为get
+        headers: {//设置请求头
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        },
+        body: JSON.stringify(data)//post参数字符串
+    }, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var list = []
+            JSON.parse(body).results.forEach(function (v) {
+                list.push({
+                    title: v.companyShortName,
+                    code: v.stockCode,
+                    text: v.attachedContent,
+                    id: v.esId,
+                    type: 'ShenZhen'
+                })
+            })
+            renderListFilter(list)
+            callback()
+        }else{
+            console.log(error)
+        }
+    }); 
 }
 
 
@@ -60,14 +176,46 @@ var express = require('express');
 var app = express();
 app.use('/public', express.static('public'));
 
-//  主页输出 "Hello World"
-app.get('/', function (req, res) {
-    console.log("主页 GET 请求");
-    getHtml(
+app.get('/ShangHai', function (req, res) {
+    console.log("ShangHai 交易所 请求");
+    getShangHaiHtml(
         function () {
             res.send(htmlTest);
         }
     )
+})
+
+
+app.get('/ShenZhen', function (req, res) {
+    console.log("ShenZhen 交易所 请求");
+    getShenZhenHtml(
+        function () {
+            res.send(htmlTest);
+        }
+    )
+})
+
+
+app.get('/Setting', function (req, res) {
+    console.log("设置过滤文本 请求");
+    var data = getFilter()
+    var postHTML = 
+    '<html><head><meta charset="utf-8"><title>StockMarket实例</title></head>' +
+    '<body>' +
+    '<form method="post" action="/submitSetting">' +
+    '关键词： <textarea name="name" style="width: 300px;height: 200px;">'+ data +'</textarea><br>' +
+    '<input type="submit" style="width: 100px;height: 30px; margin-top: 10px;">' +
+    '</form>' +
+    '</body></html>';
+    res.send(postHTML);
+})
+
+//  主页输出 "Hello World"
+app.get('/', function (req, res) {
+    var html = '<h1 style="text-align: center;"><a target="_blank" href="/ShangHai">上海</a></h1>'
+    html += '<h1 style="text-align: center;"><a target="_blank" href="/ShenZhen">深圳</a></h1>'
+    html += '<h1 style="text-align: center;"><a target="_blank" href="/Setting">设置过滤文本</a></h1>'
+    res.send(html);
 })
 
 
@@ -78,9 +226,32 @@ app.post('/', function (req, res) {
 })
 
 //  /del_user 页面响应
-app.get('/del_user', function (req, res) {
-    console.log("/del_user 响应 DELETE 请求");
-    res.send('删除页面');
+app.post('/submitSetting', function (req, res) {
+    console.log("设置 请求");
+    var body = "";
+    req.on('data', function (chunk) {
+        body += chunk;
+    });
+    req.on('end', function () {
+        // 解析参数
+        body = querystring.parse(body);
+        // 设置响应头部信息及编码
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
+        
+      
+        res.write("设置成功:" + body.name);
+        fs.writeFile('filter.txt', body.name,  function(err) {
+            if (err) {
+                return console.error(err);
+            }
+            console.log("数据写入成功！");
+            console.log("--------我是分割线-------------")
+            console.log("读取写入的数据！");
+        
+            });
+        
+        res.end();
+    });
 })
 
 //  /list_user 页面 GET 请求
